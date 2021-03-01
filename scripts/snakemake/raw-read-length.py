766b82c68c64c83812153ace76006a4eaec5bd30
Muhammed Hasan Celik <muhammedhasancelik@gmail.com>
2021-03-01 15:47:08 -0800

    adapted to GM12878

--- scripts/snakemake/raw-read-length.py
+++ scripts/snakemake/raw-read-length.py
@@ -1,6 +1,7 @@
 import gzip
 min_len = -1
 max_len = 0
+
 with gzip.open(snakemake.input.input_fa, "rt") as infile:
     for line in infile:
         if line.startswith(">"):
@@ -12,7 +13,8 @@ with gzip.open(snakemake.input.input_fa, "rt") as infile:
             min_len = curr_len
         if curr_len > max_len:
             max_len = curr_len
+
 with open(snakemake.output.raw_len, "w") as out, open(snakemake.input.raw_cnt, "r") as cnt:
-    out.write("%s" % cnt.read() )
+    out.write("%s" % cnt.read())
     out.write("reads.raw.length.min\t%i\n" % min_len)
     out.write("reads.raw.length.max\t%i\n" % max_len)
